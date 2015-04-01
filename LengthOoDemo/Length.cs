adfefcf7064ec7cb16651870d8fbad802464c02c
hjyao <hjyao@thoughtworks.com>
2015-04-01 13:49:21 +0800

    [xl & xn] initial length for first class of OO

---
+++ LengthOoDemo/Length.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LengthOoDemo
+{
+    class Length
+    {
+        private string unit;
+        private long value;
+
+        public Length(long value, string unit)
+        {
+            this.value = value;
+            this.unit = unit;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Length length = (Length) obj;
+            return length.value == value && unit == length.unit;
+        }
+    }
+}
---
+++ LengthOoDemo/LengthTest.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace LengthOoDemo
+{
+    public class LengthTest
+    {
+        [Fact]
+        public void should_equal_given_same_value_and_unit()
+        {
+            Assert.Equal(new Length(10, "m"), new Length(10, "m"));
+        }
+
+        [Fact]
+        public void should_not_equal_given_same_value_and_different_unit()
+        {
+            Assert.NotEqual(new Length(10, "cm"), new Length(10, "m"));
+        }
+
+        [Fact]
+        public void should_equal_given_same_length_with_different_unit()
+        {
+            Assert.Equal(new Length(100, "cm"), new Length(1, "m"));
+        }
+    }
+}
